42b1492b1b48857ad2f463b630064550cfb725a9
Bryce Yang <nash31@korea.ac.kr>
2021-06-08 19:21:49 +0900

    Integer-parseInt 기능 구현

---
+++ javaalgorithm/src/main/java/etc/IntegerUtils.java
@@ -0,0 +1,19 @@
+package etc;
+
+public class IntegerUtils {
+
+	/**
+	 * Parsing String to int
+	 * @param in
+	 * @return
+	 */
+	public static int parseInt(String in){
+		int result = 0 ;
+		int length = in.length();
+		for (int i = 0; i < length; i++) {
+			result += (int)(Character.getNumericValue(in.charAt(i)) * Math.pow(10,(length - i -1)));
+		}
+
+		return result;
+	}
+}
