4693cd79530f800c0770f930b99f4fc8f170a04a
Bryce Yang <nash31@korea.ac.kr>
2021-10-24 17:15:51 +0900

    simple dijkstra

--- javaalgorithm/src/main/java/thisalgotest/dijkstra/Dijkstra.java
+++ javaalgorithm/src/main/java/thisalgotest/dijkstra/Dijkstra.java
@@ -6,10 +6,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Dijkstra {
 
-	public void solution(int N, int V, int start, int[][] data) {
+	public int[] solution(int N, int V, int start, int[][] data) {
 		// 방문한 적이 있는지 체크하는 목적의 리스트 만들기
 		boolean[] visited = new boolean[N + 1];
 
@@ -48,6 +49,7 @@ public class Dijkstra {
 			}
 		}
 
+		return distance;
 	}
 
 	/**
@@ -69,15 +71,59 @@ public class Dijkstra {
 		return index;
 	}
 
+	public int[] simpleSolution(int N, int V, int start, int[][] data) {
+		// 방문한 적이 있는지 체크하는 목적의 리스트 만들기
+		boolean[] visited = new boolean[N + 1];
 
+		// 최단 거리 테이블을 모두 무한으로 초기화
+		int[] distance = new int[N + 1];
+		Arrays.fill(distance, Integer.MAX_VALUE);
 
-	static class Data {
+		// 각 노드에 연결되어 있는 노드에 대한 정보를 담는 그래프 만들기
+		Map<Integer, List<Data>> graph = new HashMap<>();
+		for (int[] datum : data) {
+			List<Data> orDefault = graph.getOrDefault(datum[0], new ArrayList<>());
+			orDefault.add(new Data(datum[1], datum[2]));
+			graph.put(datum[0], orDefault);
+		}
+
+		// 시작 노드로 가기 위한 최단 경로는 0으로 설정하여 큐에 삽입
+		PriorityQueue<Data> pq = new PriorityQueue<>();
+		pq.offer(new Data(start, 0));
+		distance[start] = 0;
+
+		while (!pq.isEmpty()) { // 큐가 비어있지 않다면
+			// 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+			Data poll = pq.poll();
+			// 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+			if (distance[poll.end] < poll.weight) {
+				continue;
+			}
+			// 현재 노드와 연결되 다른 인접한 노드들을 확인
+			for (Data data1 : graph.getOrDefault(poll.end, Collections.emptyList())) {
+				int cost = poll.weight + data1.weight;
+				// 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+				if (cost < distance[data1.end]) {
+					distance[data1.end] = cost;
+					pq.offer(new Data(data1.end, cost));
+				}
+			}
+		}
+		return distance;
+	}
+
+	static class Data implements Comparable{
 		public int end;
 		public int weight;
 
 		public Data(int end, int weight) {
 			this.end = end;
 			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Object o) {
+			return this.weight - ((Data)o).weight;
 		}
 	}
 
--- javaalgorithm/src/test/java/thisalgotest/dijkstra/DijkstraTest.java
+++ javaalgorithm/src/test/java/thisalgotest/dijkstra/DijkstraTest.java
@@ -1,4 +1,5 @@
 package thisalgotest.dijkstra;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -8,10 +9,21 @@ public class DijkstraTest {
 	@Test
 	public void case1() throws Exception {
 		// given
-		dij.solution(6, 11, 1,
+		assertArrayEquals(new int[]{Integer.MAX_VALUE,0, 2,3,1,2,4}, dij.solution(6, 11, 1,
 			new int[][] {{1, 2, 2}, {1, 3, 5}, {1, 4, 1}, {2, 3, 3}, {2, 4, 2}, {3, 2, 3}, {3, 6, 5}, {4, 3, 3},
-				{4, 5, 1}, {5, 3, 1}, {5, 6, 2}});
+				{4, 5, 1}, {5, 3, 1}, {5, 6, 2}}));
 
+		// when
+
+		// then
+	}
+
+	@Test
+	public void case2() throws Exception {
+		// given
+		assertArrayEquals(new int[]{Integer.MAX_VALUE,0, 2,3,1,2,4}, dij.simpleSolution(6, 11, 1,
+			new int[][] {{1, 2, 2}, {1, 3, 5}, {1, 4, 1}, {2, 3, 3}, {2, 4, 2}, {3, 2, 3}, {3, 6, 5}, {4, 3, 3},
+				{4, 5, 1}, {5, 3, 1}, {5, 6, 2}}));
 		// when
 
 		// then
